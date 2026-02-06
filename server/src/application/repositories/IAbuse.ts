3f5be205c1d4095aaa4e5752abc384744413f394
Afsal kp <afsalkp4343@gmail.com>
2026-02-06 16:39:15 +0530

    fix:merge conflict

--- server/src/application/repositories/IAbuse.ts
+++ server/src/application/repositories/IAbuse.ts
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-import { Abuse } from "../../domain/entities/Abuse";
-=======
->>>>>>> fix/eslint
 import { AbuseRequestDTO, GetAllReportsResponseDto, listOfAbsuseReposnseDTO, UpdateAbuseStatusDTO } from "../dto/AbuseDTO";
 
 export interface IAbuseUsecase{
--- server/src/application/repositories/IDatahandle.ts
+++ server/src/application/repositories/IDatahandle.ts
@@ -1,15 +1,5 @@
 import { Ticket } from "../../domain/entities/Ticket"
 import { CountResponseDTO, CountWorkspaceReponseDTO } from "../dto/DatahandleDTO"
-<<<<<<< HEAD
-import { SuperSubscriptionResponseDTO, UserDetailsResponseDTO, UserResponse } from "../dto/SuperDTO"
-export interface IDatahandleUsecase {
-fetchDataCounts():Promise<CountResponseDTO|null>
-fetchDataworkspace(limit:number,skip:number):Promise<{responseDTO:CountWorkspaceReponseDTO[],totalCount:number}>
-fetchAllUsers(limit:number,skip:number):Promise<{responseDTO:UserResponse[],totalCount:number}>
-fetchAUser(userId:string):Promise<UserDetailsResponseDTO>
-fetchSubscriptions(limit:number,skip:number):Promise<{ responseDTO:SuperSubscriptionResponseDTO [], totalDocCounts: number}>
-fetchTickets():Promise<Ticket[]>
-=======
 import { SuperSubscriptionResponseDTO, UserDetailsResponseDTO, UserResponseDTO } from "../dto/SuperDTO"
 export interface IDatahandleUsecase {
     fetchDataCounts(): Promise<CountResponseDTO | null>
@@ -18,5 +8,4 @@ export interface IDatahandleUsecase {
     fetchAUser(userId: string): Promise<UserDetailsResponseDTO>
     fetchSubscriptions(limit: number, skip: number): Promise<{ responseDTO: SuperSubscriptionResponseDTO[], totalDocCounts: number }>
     fetchTickets(): Promise<Ticket[]>
->>>>>>> fix/eslint
 }
